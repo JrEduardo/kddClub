ca838e3d9fe9e7ef9b382baa7770cd39f6e8ae47
Eduardo Junior <edujrrib@gmail.com>
2016-10-01 07:02:52 -0300

    Adiciona iscas para debugar erro no travis

--- _debug.R
+++ _debug.R
@@ -6,3 +6,31 @@ papers <- read.table("./data/papers.txt", header = TRUE, sep = ";",
 
 str(topics)
 str(papers)
+
+library(tm)
+library(rvest)
+
+higienize <- function(doc) {
+    doc %>%
+        tm_map(removeWords, stopwords("english")) %>%
+        tm_map(removePunctuation) %>%
+        tm_map(stripWhitespace) %>%
+        tm_map(removeNumbers) %>%
+        tm_map(stemDocument)
+}
+text2matrix <- function(text) {
+    ## Create Corpus
+    textdoc <- Corpus(VectorSource(text))
+    textdoc <- higienize(textdoc)
+    ## Document Term Matrix
+    textdtm <- DocumentTermMatrix(textdoc)
+    as.matrix(textdtm)
+}
+
+abstracts <- with(papers, split(abstract, topic))
+
+str(abstracts)
+str(abstracts[[1]])
+
+ma <- text2matrix(abstracts[[1]])
+str(ma)
