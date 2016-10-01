51a10b2d77b754f77e4b34d8f4964a9088c6d4ed
Eduardo Junior <edujrrib@gmail.com>
2016-10-01 04:02:11 -0300

    Adiciona arquivo R de configurações gráficas e do knitr

---
+++ _setup.R
@@ -0,0 +1,44 @@
+##======================================================================
+## Opções gerais do knitr
+library(knitr)
+library(xtable)
+opts_chunk$set(
+    warning = FALSE,
+    message = FALSE,
+    echo = TRUE,
+    tidy = FALSE,
+    fig.width = 9,
+    fig.height = 6,
+    fig.align = "center",
+    fig.pos = "h",
+    dev.args = list(family = "Palatino"))
+
+##======================================================================
+## Configura opções de gráficos do knitr
+library(lattice)
+library(latticeExtra)
+mycol <- c(1, "#377EB8", "#E41A1C", "#4DAF4A",
+           "#ff00ff", "#FF7F00", "#984EA3", "#FFFF33")
+myreg <- brewer.pal(n = 8, name = "RdGy")
+
+## Trellis graphical style.
+ps <- list(
+    box.rectangle = list(col = 1, fill = c("gray70")),
+    box.umbrella = list(col = 1, lty = 1),
+    box.dot = list(pch = "|"),
+    dot.symbol = list(col = 1, pch = 19),
+    dot.line = list(col = "gray50", lty = 3),
+    plot.symbol = list(col = 1),
+    plot.line = list(col = 1),
+    plot.polygon = list(col = "gray95"),
+    superpose.line = list(col = mycol, lty = 1),
+    superpose.symbol = list(col = mycol, pch = 1),
+    superpose.polygon = list(col = mycol),
+    strip.background = list(col = c("gray90", "gray70")),
+    regions = list(col = list(col = colorRampPalette(myreg)(100)))
+    )
+
+trellis.par.set(ps)
+
+## Remove objects
+rm(list = c("mycol", "myreg", "ps"))
